76204ff59ed853fb0b3942a7c087179843fb7e30
toanngosy <toanngosy@gmail.com>
2023-03-30 23:54:46 -0700

    update workflow

--- check_workflow.py
+++ check_workflow.py
@@ -8,9 +8,11 @@ now = datetime.now()
 api = wandb.Api()
 latest_report = api.reports(path=PROJECT_WANDB).next()
 latest_report_time = datetime.strptime(latest_report.updated_at, '%Y-%m-%dT%H:%M:%S')
-if latest_report_time < now:
-    time.sleep(10)
-    latest_report = api.reports(path=PROJECT_WANDB).next()
-    latest_report_time = datetime.strptime(latest_report.updated_at, '%Y-%m-%dT%H:%M:%S')
-else:
-    print(latest_report.url)
+while True:
+    if latest_report_time < now:
+        time.sleep(10)
+        latest_report = api.reports(path=PROJECT_WANDB).next()
+        latest_report_time = datetime.strptime(latest_report.updated_at, '%Y-%m-%dT%H:%M:%S')
+    else:
+        print(latest_report.url)
+        break
