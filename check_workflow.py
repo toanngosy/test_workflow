0fc14fb33a9de6178f1af119f58b4f0e6e37feed
toanngosy <toanngosy@gmail.com>
2023-04-20 22:34:21 -0700

    update workflow

--- check_workflow.py
+++ check_workflow.py
@@ -1,7 +1,8 @@
-from datetime import datetime
+import datetime
 import time
 from dotenv import load_dotenv
 import os
+import pytz
 from github import Github, GithubException
 
 
@@ -23,16 +24,17 @@ def get_report(repo, github_branch, github_report_path):
         content = repo.get_contents(github_report_path, ref=github_branch)
         file_sha = content.sha
         file_last_modified = content.last_modified
-        print(f'File {github_report_path} exists in branch {github_branch}.')
     except:
         file_sha = None
         file_last_modified = None
-        print(f'File {github_report_path} does not exist in branch {github_report_path}. Creating new file...')
     return file_sha, file_last_modified
 
 
 if __name__ == '__main__':
-    now = datetime.now()
+    #gmt = pytz.timezone('GMT')
+    #now = datetime.datetime.now(gmt)
+    #gmt = pytz.timezone('GMT')
+    now = datetime.datetime.now()
     load_dotenv()
     github_token = os.environ.get('TOKEN')
     github_repo = os.environ.get('REPO')
@@ -46,14 +48,16 @@ if __name__ == '__main__':
     file_sha, file_last_modified = get_report(repo, github_branch, github_report_path)
     latest_report_time = None
     if file_last_modified:
-        latest_report_time = datetime.strptime(file_last_modified, '%a, %d %b %Y %H:%M:%S GMT')
+        #latest_report_time = datetime.datetime.strptime(file_last_modified, '%a, %d %b %Y %H:%M:%S GMT').astimezone(gmt)
+        latest_report_time = datetime.datetime.strptime(file_last_modified, '%a, %d %b %Y %H:%M:%S GMT')
     while True:
         if not latest_report_time or latest_report_time < now:
             time.sleep(10)
             file_sha, file_last_modified = get_report(repo, github_branch, github_report_path)
             latest_report_time = None
             if file_last_modified:
-                latest_report_time = datetime.strptime(file_last_modified, '%a, %d %b %Y %H:%M:%S GMT')
+                #latest_report_time = datetime.datetime.strptime(file_last_modified, '%a, %d %b %Y %H:%M:%S GMT').astimezone(gmt)
+                latest_report_time = datetime.datetime.strptime(file_last_modified, '%a, %d %b %Y %H:%M:%S GMT')
         else:
             file_url = f'https://github.com/{github_repo}/blob/{file_sha}/{github_report_path}'
             print(f'Report link: {file_url}')
