45e44a734f2f18f5ff8e100f601077b1e47332c4
toanngosy <toanngosy@gmail.com>
2023-04-20 22:04:54 -0700

    update workflow

--- check_workflow.py
+++ check_workflow.py
@@ -44,12 +44,16 @@ if __name__ == '__main__':
     create_or_get_branch(repo, github_branch)
     
     file_sha, file_last_modified = get_report(github_repo, github_branch, github_report_path)
-    latest_report_time = datetime.strptime(file_last_modified, '%a, %d %b %Y %H:%M:%S GMT')
+    latest_report_time = None
+    if file_last_modified:
+        latest_report_time = datetime.strptime(file_last_modified, '%a, %d %b %Y %H:%M:%S GMT')
     while True:
-        if latest_report_time < now:
+        if not latest_report_time or latest_report_time < now:
             time.sleep(10)
             file_sha, file_last_modified = get_report(github_branch, github_report_path)
-            latest_report_time = datetime.strptime(file_last_modified, '%a, %d %b %Y %H:%M:%S GMT')
+            latest_report_time = None
+            if file_last_modified:
+                latest_report_time = datetime.strptime(file_last_modified, '%a, %d %b %Y %H:%M:%S GMT')
         else:
             file_url = f'https://github.com/{github_repo}/blob/{file_sha}/{github_report_path}'
             print(f'Report link: {file_url}')
