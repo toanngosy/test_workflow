cdc43f6ad2acbf802655a1b52bc3cd24b4c6da07
toanngosy <toanngosy@gmail.com>
2023-06-06 08:44:14 -0700

    update gen content into folder and cancel workflow

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ import yaml
 from dotenv import load_dotenv
 from github import Github, GithubException
 
-RUN_GITHUB = 'Manual test'
-PROJECT_WANDB = 'test_workflow'
-
 
 def run_data_intensive_process():
     result = random.randint(1, 100)
@@ -22,7 +19,7 @@ def generate_content(result):
     return content
 
 
-def create_or_get_branch(repo, github_branch):
+def _create_or_get_branch(repo, github_branch):
     try:
         branch = repo.get_branch(github_branch)
     except GithubException:
@@ -35,7 +32,7 @@ def create_or_get_branch(repo, github_branch):
         repo.create_git_ref(f'refs/heads/{github_branch}', base_commit.sha)
 
 
-def get_report(repo, github_branch, github_report_path):
+def _get_report(repo, github_branch, github_report_path):
     try:
         content = repo.get_contents(github_report_path, ref=github_branch)
         file_sha = content.sha
@@ -48,18 +45,9 @@ def get_report(repo, github_branch, github_report_path):
     return file_sha, file_last_modified
 
 
-def run_and_push_report(func, *args, **kwargs):
-    load_dotenv()
-    github_token = os.environ.get('TOKEN')
-    github_repo = os.environ.get('REPO')
-    github_branch = os.environ.get('BRANCH')
-    github_report_path = os.environ.get('REPORT_PATH')
-
-    g = Github(github_token)
-    repo = g.get_repo(github_repo)
-    
-    create_or_get_branch(repo, github_branch)
-    file_sha, _ = get_report(repo, github_branch, github_report_path)
+def run_and_push_report(func, *args, **kwargs): 
+    _create_or_get_branch(repo, github_branch)
+    file_sha, _ = _get_report(repo, github_branch, github_report_path)
 
     # update or create file content
     result = func(*args, **kwargs)
@@ -75,6 +63,16 @@ def run_and_push_report(func, *args, **kwargs):
     return new_file_sha
 
 
+def cancel_workflow(id):
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {github_token}",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    url = f'https://api.github.com/repos/{github_repo}/actions/runs/{id}/cancel'
+    response = requests.post(url, headers=headers)
+    return response.status_code
+    
 if __name__ == '__main__':
     # read config file
     with open('config.yaml') as f:
@@ -82,6 +80,13 @@ if __name__ == '__main__':
     
     run_name = config.get('run_name')
     machine_name = config.get('machine_name')
+    load_dotenv()
+    github_token = os.environ.get('TOKEN')
+    github_repo = os.environ.get('REPO')
+    github_branch = os.environ.get('BRANCH')
+    github_report_path = os.environ.get('REPORT_PATH').format(machine_name)
+    g = Github(github_token)
+    repo = g.get_repo(github_repo)
     
     r = requests.get('https://api.github.com/repos/toanngosy/test_workflow/actions/runs')
     r.status_code
@@ -89,10 +94,11 @@ if __name__ == '__main__':
     if len(workflow_runs):
         for run in workflow_runs:
             try:
-                gh_run_name, gh_machine_name, gh_run_actor = run.split('_')                
+                gh_run_name, gh_machine_name, gh_run_actor = run['name'].split('_')                
                 if (gh_run_name == run_name 
                     and gh_machine_name == machine_name
                     and run['status'] == 'in_progress'):
+                        cancel_workflow(run['id'])
                         run_and_push_report(run_data_intensive_process)
                         continue
             except ValueError:
